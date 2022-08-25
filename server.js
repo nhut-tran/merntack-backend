2c6c9013a73c21064988fe27bf0d393f00ac5f17
nhut-tran <trannhut1707@gmail.com>
2022-08-25 08:34:37 +0700

    Add index.html

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ require('dotenv').config();
 
 
 const app = express();
+
 app.use(cors())
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: __dirname });
+});
 app.use(express.json());
+
 app.use('/api/countries', router);
 
 mongoose.connect(process.env.MONGODB_CONNECT).then(() => {
